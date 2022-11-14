0e27793aac0a0724dacc23935be83508a82878e3
Matthew Davidson <mdavidson982@g.rwu.edu>
2022-11-13 23:39:20 -0500

    Functioning Button!

--- Teston/Teston.py
+++ Teston/Teston.py
@@ -1,20 +1,38 @@
-import discord
+import discord, json
+from discord.ext import commands
+
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(intents=intents, command_prefix="-")
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run('MTA0MTQ0MjcwMTI3NjYxODg5Mg.G3JFaT.7IM--DgpPl3pj7wJRtdfCd7zEJtK-ATjPBvpSY')
+    await bot.process_commands(message)
+
+class Buttons(discord.ui.View):
+    def __init__(self, *, timeout = 180):
+        super().__init__(timeout=timeout)
+    @discord.ui.button(label="Button", style=discord.ButtonStyle.gray)
+    async def gray_button(self,interaction:discord.Interaction,button:discord.ui.Button):
+        await interaction.response.edit_message(content=f"This is a new message")
+
+@bot.command()
+async def button(ctx):
+    print("Running")
+    await ctx.reply("This message has buttons!",view=Buttons())
+
+
+bot.run('MTA0MTQ0MjcwMTI3NjYxODg5Mg.G3JFaT.7IM--DgpPl3pj7wJRtdfCd7zEJtK-ATjPBvpSY')
