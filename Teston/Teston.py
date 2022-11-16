663caa28655857ea02983460b0efd3947627e24c
Matthew Davidson <mdavidson982@g.rwu.edu>
2022-11-15 19:25:36 -0500

    Button Changes on Press, Sends a new message.

--- Teston/Teston.py
+++ Teston/Teston.py
@@ -29,12 +29,23 @@ class Buttons(discord.ui.View):
         super().__init__(timeout=timeout)
     @discord.ui.button(label="Button", style=discord.ButtonStyle.gray)
     async def gray_button(self,interaction:discord.Interaction,button:discord.ui.Button):
-        await interaction.response.edit_message(content=f"This is a new message")
+        await interaction.response.send_message(content=f"This is a new message")
+
+
+class TestButtons(discord.ui.View):
+    def __inti__(self,*,timeout=180):
+        super().__inti__(timeout=timeout)
+    @discord.ui.button(label="New Button!", style=discord.ButtonStyle.gray)
+    async def gray_button(self,interaction:discord.Interaction,button:discord.ui.Button):
+        button.style = discord.ButtonStyle.red
+        button.disabled = True
+        await interaction.response.edit_message(content = "Borked" , view=self)
+        await interaction.followup.send(content = "Button pressed.", ephemeral = True)
 
 @bot.command()
 async def button(ctx):
     print("Running")
-    await ctx.reply("This message has buttons!",view=Buttons())
+    await ctx.reply("This message has buttons!",view=TestButtons())
 
 
 bot.run('MTA0MTQ0MjcwMTI3NjYxODg5Mg.G3JFaT.7IM--DgpPl3pj7wJRtdfCd7zEJtK-ATjPBvpSY')
