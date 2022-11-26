64b2a0e3b510cafaa42836a744b4a230ddccbb80
Matthew Davidson <mdavidson982@g.rwu.edu>
2022-11-26 15:35:33 -0500

    Functioning Weather Cog. Less Spaghetti

--- Mini Weston/modules/weather.py
+++ Mini Weston/modules/weather.py
@@ -9,13 +9,126 @@ class WeatherClass(commands.Cog):
     def __inti__(self, bot):
         self.bot = bot
 
+    #Gets the weather based off a users input
+    #Input format: -weather City name, State code (if in US), Country code
+    #Country code refers to the ISO 1833 country code list
     @commands.command(name = "weather")
-    async def weatherCommand(self, ctx, *args):
-        await weather(ctx, *args)
+    async def weather(self, ctx, *args):
+        #The first steps of this command take the user input and converts the given city to it's coresponding Lattitude and Logittude based using the OpenWeather API
+    
+        #BaseURL for getting Lat and Lon
+        baseURL = "http://api.openweathermap.org/geo/1.0/direct?q=" 
+
+        #technically the command can take an infinite number of arguments, this is because some cities can be made up of more then one name
+        #Ex: West Warwick
+        #If this we didn't do it this way, citynames containing more then one word would have to be contained using " "
+        #Which isn't user friendly
+
+
+        #Using the arguments, we reduce any extra spaces by combining each argument with a single space
+        #Ex: West                     Warwick,              RI, US
+        #Becomes: West Warwick, RI, US
+        inputArgsReader = ' '.join(args)
+
+        #Next, we create an array by splitting each input by using ,
+        positionArray = inputArgsReader.split(",")
+    
+
+        #If the positionArray is = 3, then it is in the US (since only US locations use the state codes)
+        if(len(positionArray) == 3):
+            url = baseURL + positionArray[0] + "," + positionArray[1] + "," + positionArray[2] + "&limit=10&appid=" + api_key
+            response = requests.get(url)
+            print(url)
+
+        #If the positionArray is = 2, it could be anywhere, The second input could be a state or a country code.
+        elif(len(positionArray) == 2):
+            url = baseURL + positionArray[0] + "," + positionArray[1] + "&limit=10&appid=" + api_key
+            response = requests.get(url)
+
+        #If the positionArray is 1, then the user just entered a City name. This could be dangerous? The output might not be the city they were looking for
+        elif(len(positionArray) == 1):
+
+            url = baseURL + positionArray[0] + "&limit=10&appid=" + api_key
+            response = requests.get(url)
+            print("Geo URL: "+url)
+        
+    
+        #If something goes wrong here, The user input was incorrect. Throw an Excetion
+    
+        file = response.json()
+
+   
+        print("Response Length: "+ str(len(file)))
+
+        try:
+             #if no results, say that nothing was found
+            if (len(file) == 0):
+                await ctx.send("Dumbass")
+
+
+            #If one result, send the information directly to the getWeather function, no selection needed
+            elif len(file) == 1:
+                    place = file[0]
+
+                    #if there is a state in the location, send it to getWeather, if there is no state the stateName variable in the function defaults to ""
+                    if "state" in place:
+                        await ctx.send(embed = getWeather(place["lat"],place["lon"],place["name"],place["state"]))
+                    else:
+                        await ctx.send(embed = getWeather(place["lat"],place["lon"],place["name"]))
+
+
+            #if more then one result, create a selection view where the user will be asked
+            #to select on of the results. After an option is selected, display the weather for that city.
+            else:
+                #Creates a DropDownView object
+                view = DropdownView()
+
+                #list of new and duplicate country and states.
+                #This is used to make sure duplicates aren't put into the selection list
+                newState = []
+                newCountry = []
+                dupState = []
+                dupCountry = []
+                for i in range(len(file)):
+                    place = file[i]
+
+                    #if this instance of file mentions a state
+                    if "state" in place:
+
+                        #if the state has already been added to the newState list, do not create a new dropdown
+                        if place["state"] not in newState:
+                            newState.append(place["state"])
+
+
+                            if(emoji.is_emoji(flag_for(place["country"]))):
+                                #dropdown creation
+                                view.dropdown.add_option(label = place["name"] + ", " + place["state"] + ", " + place["country"], emoji = flag_for(place["country"]))
+                            else:
+                                #dropdown creation for places without flag emoji
+                                view.dropdown.add_option(label = place["name"] + ", " + place["state"] + ", " + place["country"])
+                        else:
+                            dupState.append(i)
+                
+                    #if a state is not mentioned, try to find duplicate countries
+                    else:
+                        if place["country"] not in newCountry:
+                            newCountry.append(i)
+                            if(emoji.is_emoji(flag_for(place["country"]))):
+                                view.dropdown.add_option(label = place["name"] + ", " + place["country"], emoji = flag_for(place["country"]))
+                            else:
+                                view.dropdown.add_option(label = place["name"] + ", " + place["country"])
+                        else:
+                            dupCountry.append(i)
+
+                await ctx.send("**multiple results found:**", view = view)
+
+        except:
+             await ctx.send("Something went wrong. Did you put the command in correctly?")
 
 
 async def setup(bot):
     await bot.add_cog(WeatherClass(bot))
+
 
 def flag_for(code):
     """Return unicode flag emoji given a 2-digit country code."""
@@ -154,117 +267,3 @@ def getWeather(lat,lon,cityName,stateName = ""):
         return embed
 
 
-#Gets the weather based off a users input
-#Input format: -weather City name, State code (if in US), Country code
-#Country code refers to the ISO 1833 country code list.
-async def weather(ctx, *args):
-    #The first steps of this command take the user input and converts the given city to it's coresponding Lattitude and Logittude based using the OpenWeather API
-    
-    #BaseURL for getting Lat and Lon
-    baseURL = "http://api.openweathermap.org/geo/1.0/direct?q=" 
-
-    #technically the command can take an infinite number of arguments, this is because some cities can be made up of more then one name
-    #Ex: West Warwick
-    #If this we didn't do it this way, citynames containing more then one word would have to be contained using " "
-    #Which isn't user friendly
-
-
-    #Using the arguments, we reduce any extra spaces by combining each argument with a single space
-    #Ex: West                     Warwick,              RI, US
-    #Becomes: West Warwick, RI, US
-    inputArgsReader = ' '.join(args)
-
-    #Next, we create an array by splitting each input by using ,
-    positionArray = inputArgsReader.split(",")
-    
-
-    #If the positionArray is = 3, then it is in the US (since only US locations use the state codes)
-    if(len(positionArray) == 3):
-        url = baseURL + positionArray[0] + "," + positionArray[1] + "," + positionArray[2] + "&limit=10&appid=" + api_key
-        response = requests.get(url)
-        print(url)
-
-    #If the positionArray is = 2, it could be anywhere, The second input could be a state or a country code.
-    elif(len(positionArray) == 2):
-        url = baseURL + positionArray[0] + "," + positionArray[1] + "&limit=10&appid=" + api_key
-        response = requests.get(url)
-
-    #If the positionArray is 1, then the user just entered a City name. This could be dangerous? The output might not be the city they were looking for
-    elif(len(positionArray) == 1):
-
-        url = baseURL + positionArray[0] + "&limit=10&appid=" + api_key
-        response = requests.get(url)
-        print("Geo URL: "+url)
-        
-    
-    #If something goes wrong here, The user input was incorrect. Throw an Excetion
-    
-    file = response.json()
-
-   
-    print("Response Length: "+ str(len(file)))
-
-    try:
-         #if no results, say that nothing was found
-        if (len(file) == 0):
-            await ctx.send("Dumbass")
-
-
-        #If one result, send the information directly to the getWeather function, no selection needed
-        elif len(file) == 1:
-                place = file[0]
-
-                #if there is a state in the location, send it to getWeather, if there is no state the stateName variable in the function defaults to ""
-                if "state" in place:
-                    await ctx.send(embed = getWeather(place["lat"],place["lon"],place["name"],place["state"]))
-                else:
-                    await ctx.send(embed = getWeather(place["lat"],place["lon"],place["name"]))
-
-
-        #if more then one result, create a selection view where the user will be asked
-        #to select on of the results. After an option is selected, display the weather for that city.
-        else:
-            #Creates a DropDownView object
-            view = DropdownView()
-
-            #list of new and duplicate country and states.
-            #This is used to make sure duplicates aren't put into the selection list
-            newState = []
-            newCountry = []
-            dupState = []
-            dupCountry = []
-            for i in range(len(file)):
-                place = file[i]
-
-                #if this instance of file mentions a state
-                if "state" in place:
-
-                    #if the state has already been added to the newState list, do not create a new dropdown
-                    if place["state"] not in newState:
-                        newState.append(place["state"])
-
-
-                        if(emoji.is_emoji(flag_for(place["country"]))):
-                            #dropdown creation
-                            view.dropdown.add_option(label = place["name"] + ", " + place["state"] + ", " + place["country"], emoji = flag_for(place["country"]))
-                        else:
-                            #dropdown creation for places without flag emoji
-                            view.dropdown.add_option(label = place["name"] + ", " + place["state"] + ", " + place["country"])
-                    else:
-                        dupState.append(i)
-                
-                #if a state is not mentioned, try to find duplicate countries
-                else:
-                    if place["country"] not in newCountry:
-                        newCountry.append(i)
-                        if(emoji.is_emoji(flag_for(place["country"]))):
-                            view.dropdown.add_option(label = place["name"] + ", " + place["country"], emoji = flag_for(place["country"]))
-                        else:
-                            view.dropdown.add_option(label = place["name"] + ", " + place["country"])
-                    else:
-                        dupCountry.append(i)
-
-            await ctx.send("**multiple results found:**", view = view)
-
-    except:
-         await ctx.send("Something went wrong. Did you put the command in correctly?")
