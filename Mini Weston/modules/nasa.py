21b57ed8fedce538b578cd38822672065e1476b7
mdavidson982 <mdavidson982@g.rwu.edu>
2023-11-29 20:00:42 -0500

    Functional NASA description Page. No Return.

--- Mini Weston/modules/nasa.py
+++ Mini Weston/modules/nasa.py
@@ -1,12 +1,31 @@
 import discord, requests, json, aiohttp
 from discord.ext import commands
 
-#Start of SQL testing
+#NASA Buttons
+class ReadDescription(discord.ui.View):
+    def __init__(self, json, timeout = 180):
+        super().__init__(timeout=timeout)
+        self.js = json
+
+    # When the confirm button is pressed, set the inner value to `True` and
+    # stop the View from listening to more input.
+    # We also send the user an ephemeral message that we're confirming their choice.
+    @discord.ui.button(label='Read Description', style=discord.ButtonStyle.green, )
+    async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
+        apodDescriptionEmbed = discord.Embed(title = "**" + self.js["title"] + "**")
+        apodDescriptionEmbed.set_footer(text=self.js["explanation"])
+        apodDescriptionEmbed.set_thumbnail(url= "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/NASA_Worm_logo.svg/2560px-NASA_Worm_logo.svg.png")
+        await interaction.response.edit_message(embed = apodDescriptionEmbed)
+
+    # This one is similar to the confirmation button except sets the inner value to `False`
+    @discord.ui.button(label='Cancel', style=discord.ButtonStyle.grey)
+    async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message('Cancelling', ephemeral=True)
+
 class nasa(commands.Cog):
     def __init__(self,bot):
         self.bot=bot
         self.API_KEY = "0XlU1S3AcEWP6Is3X30bqGgBT9Hx04UcYsW6k1N1"
-        #print("Yeah, nada loaded")
 
     @commands.command()
     async def apod(self,ctx):
@@ -17,19 +36,16 @@ class nasa(commands.Cog):
                 self.js = await r.json()
         
         if(self.js["media_type"] == "image"):
-            apodEmbed = discord.Embed(title = "**" + self.js["title"] + "**")
-            
-            #apodEmbed.set_author(self.js["date"])
+            apodImageEmbed = discord.Embed(title = "**" + self.js["title"] + "**")
+
             if("hdurl" in self.js):
-                apodEmbed.set_image(url = self.js["hdurl"])
-                apodEmbed.set_footer(text="NASA Astronomy Picture of the Day", icon_url= "https://cdn.discordapp.com/attachments/849172801076199495/1049776014131200021/nasa-logo-web-rgb.png")
+                apodImageEmbed.set_image(url = self.js["hdurl"])
+                apodImageEmbed.set_footer(text = "NASA Astronomy Picture of the Day | Date: {date}".format(date = self.js["date"]), icon_url= "https://cdn.discordapp.com/attachments/849172801076199495/1049776014131200021/nasa-logo-web-rgb.png")
             else:
-                apodEmbed.set_image(self.js["url"])
-                apodEmbed.set_footer("NASA Astronomy Picture of the Day", icon_url= "https://cdn.discordapp.com/attachments/849172801076199495/1049776014131200021/nasa-logo-web-rgb.png")
-        
+                apodImageEmbed.set_image(self.js["url"])
+                apodImageEmbed.set_footer(text = "NASA Astronomy Picture of the Day | Date: {date} |".format(date = self.js["date"]), icon_url= "https://cdn.discordapp.com/attachments/849172801076199495/1049776014131200021/nasa-logo-web-rgb.png")
 
-        await ctx.send(embed=apodEmbed)
-
+        await ctx.send(embed=apodImageEmbed, view = ReadDescription(self.js))
 
 
 
